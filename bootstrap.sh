6284e1eea6236d9ea982a74fc34a76bcee459f9c
Mikael Gibert <mikael.gibert@numergy.com>
2015-11-17 18:38:54 +0100

    Adding more tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,6 @@
 sudo apt-get install -y python-pip
 sudo pip install -U ansible
 
-sudo ansible-galaxy install innohub-ansible.ruby-install --force
-sudo ansible-galaxy install malk.java8-oracle --force
-sudo ansible-galaxy install thydel.vagrant --force
+sudo ansible-galaxy install malk.java8-oracle thydel.vagrant migibert.terraform --force
 
 ansible-playbook -i localhost, workstation.yml --connection=local
