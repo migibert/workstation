a9594effade55efa8e59fde85daf6c5aa5345824
Mikael Gibert <mikael.gibert@gmail.com>
2015-11-17 23:16:33 +0100

    Install docker and tag IDE installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 sudo apt-get install -y python-pip
 sudo pip install -U ansible
 
-sudo ansible-galaxy install malk.java8-oracle thydel.vagrant migibert.terraform --force
+sudo ansible-galaxy install malk.java8-oracle thydel.vagrant migibert.terraform angstwad.docker_ubuntu --force
 
 ansible-playbook -i localhost, workstation.yml --connection=local
