a1b0987160a97e387def096e97e9337bbb50912b
Mikael Gibert <mikael.gibert@numergy.com>
2015-11-25 17:57:55 +0100

    Add development directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,6 @@
 sudo apt-get install -y python-pip
 sudo pip install -U ansible
 
-sudo ansible-galaxy install malk.java8-oracle thydel.vagrant migibert.terraform angstwad.docker_ubuntu --force
+sudo ansible-galaxy install malk.java8-oracle thydel.vagrant migibert.terraform angstwad.docker_ubuntu playlist.go --force
 
-ansible-playbook -i localhost, workstation.yml --connection=local
+ansible-playbook -i localhost, workstation.yml --connection=local --extra-vars "dev_user=$(whoami)"
