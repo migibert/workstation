0669d3e8c289ff1f0f212a8f19eeb57de253ed48
Mikael Gibert <mikael.gibert@gmail.com>
2017-02-06 00:13:54 +0100

    Update tools versions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y python-pip
+sudo apt-get install -y build-essential python-pip libffi-dev libssl-dev 
 sudo pip install -U ansible
 
-sudo ansible-galaxy install malk.java8-oracle thydel.vagrant migibert.terraform angstwad.docker_ubuntu playlist.go --force
+sudo ansible-galaxy install thydel.vagrant migibert.terraform angstwad.docker_ubuntu jacobwgillespie-ansible.go --force
 
 ansible-playbook -i localhost, workstation.yml --connection=local --extra-vars "dev_user=$(whoami)"
